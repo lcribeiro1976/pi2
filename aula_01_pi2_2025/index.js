4d45d819aec7de64833fd12e15ef1d82734c02ce
lcribeiro1976 <leonardocaetanoribeiro@gmail.com>
2025-08-11 10:16:02 -0300

    Create index.js

---
+++ aula_01_pi2_2025/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.json({message: 'Hello World'});
+});
+
+//Rota com parâmetro na URL
+app.get('/hello/:nome', (req, res) => {
+    const nome = req.params.nome;
+    res.json({message: `Hello ${nome}!`});
+});
+
+//Rota com parâmetros na query string
+app.get('/pesquisa', (req, res) => {
+    const nome = req.query.nome;
+    const sobrenome = req.query.sobrenome;
+    const mensagem = `${nome} ${sobrenome}`;
+    res.json({message: `Hello ${mensagem}`});
+    //Exemplo de rota
+    //http://localhost:3000/pesquisa?nome=André&sobrenome=Batista
+});
+
+app.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}`);
+});
